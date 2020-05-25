dc0cf1c755230b5e72a234bc6321f4712a6acdb9
Thordreck <paezguerraalvaro@gmail.com>
2020-05-25 23:16:19 +0200

    Added RateLimiter struct in clock module and related tests.

--- src/clock.rs
+++ src/clock.rs
@@ -1,7 +1,7 @@
 
-use std::time::Duration;
+use std::time::{ Duration, Instant };
 
-#[derive(Debug, PartialEq)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Frequency { hertz: f64 }
 
 impl Frequency
@@ -30,10 +30,47 @@ impl Frequency
 pub const DEFAULT_CPU_FREQUENCY    : Frequency = Frequency { hertz: 500.0 };
 pub const DEFAULT_TIMERS_FREQUENCY : Frequency = Frequency { hertz: 60.0 };
 
+pub struct RateLimiter<F>
+    where F: FnMut() -> Result<(), String>
+{
+    max_rate : Frequency,
+    function : F,
+    last_executed: Option<Instant>,
+}
+
+impl<F: FnMut() -> Result<(), String>> RateLimiter<F>
+{
+    pub fn new(function: F, max_rate: Frequency) -> Self
+    {
+        RateLimiter { max_rate, function, last_executed: None }
+    }
+
+    pub fn execute(&mut self) -> Result<(), String>
+    {
+        if self.last_executed.is_none()
+        {
+            self.last_executed = Some(Instant::now());
+        }
+
+        let now     = Instant::now();
+        let elapsed = now.duration_since(self.last_executed.unwrap());
+
+        if elapsed >= self.max_rate.period()
+        {
+            self.last_executed = Some(now);
+            return (self.function)();
+        }
+
+        Ok(())
+    }
+}
+
+
 #[cfg(test)]
 mod tests
 {
     use super::*;
+    use std::time::SystemTime;
 
     #[test]
     fn frequency()
@@ -75,5 +112,43 @@ mod tests
         assert_eq!(frequency.period(), Duration::from_millis(0),
                    "0hz period is not equal to 0ms");
 
+    }
+
+    #[test]
+    fn rate_limiter() -> Result<(), String>
+    {
+        let frequency        = Frequency::new(1000.0);
+        let lambda_frequency = frequency;
+        let test_time        = Duration::from_millis(100);
+
+        let mut counter = 1;
+
+        let lambda = ||
+        {
+            counter += 1;
+            if counter > (test_time.as_millis() / lambda_frequency.period().as_millis())
+            {
+                return Err(String::from("Closure was called too many times. \
+                                         Max frequency was not respected"));
+            }
+
+            Ok(())
+        };
+
+
+        let mut rate_limiter = RateLimiter::new(lambda, frequency);
+
+        let start_time = SystemTime::now();
+
+        while SystemTime::now().duration_since(start_time).unwrap() <= test_time
+        {
+            rate_limiter.execute()?;
+        }
+
+        let expected_value = test_time.as_millis() / frequency.period().as_millis();
+        assert_eq!(counter, expected_value, "Closure was not executed the expected \
+                   number of times given the frequency and test time");
+
+        Ok(())
     }
 }
